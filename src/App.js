3ae7539eb996cc095df3883858d473199a16b8f8
CamHaoHa <hao@pedalverse.io>
2023-04-09 21:41:22 +1000

    feat: add Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          <p>Content</p>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
